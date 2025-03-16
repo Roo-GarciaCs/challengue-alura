2cb735efbda32346dbfdf92593de62cdd275e096
Roberto García <rock.n.roa@gmail.com>
2025-03-16 01:24:27 -0600

    Versión final final
    El proyecto se ha logrado :)

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const listaDeAmigos = [];
 const ulListaDeAmigos = document.getElementById ("listaAmigos");
 const listaDeResultados = document.getElementById("resultado");
 
-function agregarAmigo(){
-    if(!nombreAmigo.value){
-        alert("Debes de ingresar un nombre")
-    } else if (!/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]*$/.test(nombreAmigo.value)) {
-        alert("Valor no permitido, introduzca un nombre")
-        .return
+function agregarAmigo() {
+    if (!nombreAmigo.value.trim()) { //trim() comprueba si hay espacios en blanco
+        alert("Debes de ingresar un nombre");
+    } else if (!/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]*$/.test(nombreAmigo.value)) { //verifica si hay números
+        alert("Valor no permitido, introduzca un nombre");
+    } else {
+        listaDeAmigos.push(nombreAmigo.value);
+        ulListaDeAmigos.innerHTML += `<li>${nombreAmigo.value}</li>`;
+        nombreAmigo.value = ""; 
     }
- listaDeAmigos.push (nombreAmigo.value);
- ulListaDeAmigos.innerHTML += `<li>${nombreAmigo.value}</li>`;
- nombreAmigo.value = "";
 }
 
 function sortearAmigo(){
